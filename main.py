856d024a1b3df7a5f5d09e4d26e61154ed6d8ccb
Hradisky <148043932+Hradisky@users.noreply.github.com>
2023-10-23 09:24:18 +0200

    cecko

--- main.py
+++
@@ -1,9 +0,0 @@
-while True:
-    text = input()
-    text_c = str.lower(text)
-    text_backwards = text_c[::-1]
-
-    if text_backwards == text_c:
-        print("ano")
-    else:
-        print("nie")
---
+++ uvod.c
@@ -0,0 +1,31 @@
+///////////////////////////// Fes Dobla vizitka
+#include <stdio.h>
+
+int main(){
+    /* printf("**********************\n");
+    printf("*                    *\n");
+    printf("*                    *\n");
+    printf("*       Branik       *\n");
+    printf("*                    *\n");
+    printf("*                     \n");
+    printf("**********************\n"); */
+    /* int cislo1;
+    int cislo2;
+    int v;
+
+    cislo1 = 12;
+    cislo2 = 45;
+    v = cislo1 + cislo2;
+    printf("Suces cisel %d a %d je %d", cislo1,cislo2, cislo1 + cislo2); */
+    int a;
+    int b;
+
+    printf("Cski, kalkucka som lmao\n");
+    printf("Dafaj mi cislo 1\n");
+    scanf("%d",&a);
+    printf("Dafaj m icislo 2\n");
+    scanf("%d",&b);
+    printf("Sucet cisel %d a %d je %d", a,b,a+b);
+}
+
+///////////////////////////
